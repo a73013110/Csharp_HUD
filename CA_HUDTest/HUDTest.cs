05810c2aaae19a2e87082fd68dff2d26ceb0d213
王義凱 <a73013110@gmail.com>
2019-07-26 14:44:07 +0800

    update

--- CA_HUDTest/HUDTest.cs
+++ CA_HUDTest/HUDTest.cs
@@ -33,7 +33,28 @@ namespace CA_HUDTest
             appthread.Start();
             // 暫停程序, 當(app != null)繼續執行(怕別的執行序還沒new Application), 若逾時則強制繼續執行(後面可能引發錯誤)
             SpinWait.SpinUntil(() => (app != null), 1000);
-            app.Dispatcher.Invoke(() => new HUD().Show());
+            app.Dispatcher.Invoke(() => {
+                HUD hud = new HUD();
+                _ = AccordingTimeToChangeValue(hud);
+                hud.Show();
+                hud.StartDuration();
+            });
+        }
+
+        /// <summary>
+        /// Change title and message according time
+        /// </summary>
+        private static async Task AccordingTimeToChangeValue(HUD hud)
+        {
+            int durationTime = 3;
+            string[] msgTitle = { "螢幕即將關閉", "螢幕即將關閉", "螢幕即將關閉" };
+            string[] msgDetail = { "剩餘3秒", "剩餘2秒", "剩餘1秒" };
+            hud.Message.MsgDuration = 3;
+            for (int i = 0; i < durationTime; i++)
+            {
+                hud.SetMsg(msgTitle[i], msgDetail[i]);
+                await Task.Delay(1000);
+            }
         }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 ## Head-up Display (HUD)
+A beautiful GUI and animation to show up notification
+## File Structure
+This solution contains three projects.
+  1. Csharp_HUD: [Main project] implement all HUD features and compile as DLL file.
+  2. WPF_HUDTest: [Test project] implement GUI button_click to open the HUD.
+  3. CA_HUDTest: [Test project] implement console application to open the HUD.
